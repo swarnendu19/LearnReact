eaeee76631fa910d922fded875842f38b9e84a24
Swarnendu <swarnendumaity633@gmail.com>
2023-09-26 21:52:42 +0530

    Update

---
+++ CurrencyConverter/src/hooks/useCurrency.js
@@ -0,0 +1,10 @@
+import { useEffect,useState } from "react";
+
+function useCurrencyInfo(currency){
+  useEffect(()=>{
+  fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+  .then((res)=> res.jason())
+
+  },[])
+}
+
