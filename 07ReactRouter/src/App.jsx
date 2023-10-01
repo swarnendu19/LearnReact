484c0297e7bb2cca6905a95ec00daaf97d6abbf6
Swarnendu <swarnendumaity633@gmail.com>
2023-10-01 11:11:56 +0530

    Update

--- 07ReactRouter/src/App.jsx
+++ 07ReactRouter/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-green-600 p-4">React Router</h1>
-    </>
+     
+     </>
   )
 }
 
---
+++ 07ReactRouter/src/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Footer'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
