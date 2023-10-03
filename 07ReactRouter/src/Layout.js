9b67025a7963d7f9769abd86bd0631e09e793b79
Swarnendu <swarnendumaity633@gmail.com>
2023-10-03 18:14:46 +0530

    update

--- 07ReactRouter/src/Layout.js
+++ 07ReactRouter/src/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Components/Footer'
+import User from './components/User/User'
 
 function Layout() {
   return (
     <>
     <Header/>
-
+    <User/>
     <Footer/>
     </>
   )
