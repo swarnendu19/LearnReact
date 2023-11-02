40aab61d364d04d9acfaccdd1a3fcedf1c64cf58
Swarnendu <swarnendumaity633@gmail.com>
2023-11-02 18:16:36 +0530

    ContextAPI

--- 08miniContext/src/Component/Profile.jsx
+++ 08miniContext/src/Component/Profile.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import UserContext from '../context/UserContext'
 
 function Profile() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const{user} = useContext(UserContext)
+if ( !user) return <div></div>
+return <div> Welcome {user.username}</div>
+   
 }
 
 export default Profile
