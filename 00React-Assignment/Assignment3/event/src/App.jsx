4b558cab1fac9c196c17901d3788bec112c92e2d
Swarnendu <swarnendumaity633@gmail.com>
2023-11-27 19:38:24 +0530

    update

--- 00React-Assignment/Assignment3/event/src/App.jsx
+++ 00React-Assignment/Assignment3/event/src/App.jsx
@@ -7,10 +7,26 @@ function App() {
     <>
       <img src="https://images.pexels.com/photos/372787/pexels-photo-372787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
       
-      <input type="text" />
+      <input type="text" onFocus={()=>{
+        console.log("Focused on the text box");
+      }}  onBlur={()=>{
+        console.log("Out of Textbox");
+      }}/>
 
-      <button  >Play</button>
-    </>
+      <button  onDoubleClick={(e)=>{
+        alert("Button CLicked")
+        console.log("Clicked");
+      }}>Play</button>
+
+      <p onCopy={()=> { alert( "Copied")}} >Hello Everyone</p>
+      <div>
+      <form action="">
+        <input type="text" />
+        <button></button>
+      </form>
+      </div>
+     </>
+
   )
 }
 
