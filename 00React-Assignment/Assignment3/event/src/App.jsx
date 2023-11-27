5b339fa82fd7f6df29498c9a8971639fcdebe65a
Swarnendu <swarnendumaity633@gmail.com>
2023-11-27 10:30:51 +0530

    Assignment3

---
+++ 00React-Assignment/Assignment3/event/src/App.jsx
@@ -0,0 +1,19 @@
+ 
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <img src="https://images.pexels.com/photos/372787/pexels-photo-372787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      
+      <input type="text" />
+
+      <button  >Play</button>
+    </>
+  )
+}
+
+export default App
