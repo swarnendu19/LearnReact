1d909d5079329b1007c548c4f5c106f5271ffecc
Swarnendu <swarnendumaity633@gmail.com>
2023-11-29 18:21:31 +0530

    Assignment

--- 00React-Assignment/Assignment3/event/src/App.jsx
+++ 00React-Assignment/Assignment3/event/src/App.jsx
@@ -2,28 +2,34 @@ import React from 'react'
 import './App.css'
 
 function App() {
+  const handleInputChange = (e)=>{
+    alert("Clicked")}
+
+    const handleSubmit =(e)=>{
+      e.preventDefault()
+      console.log("Input Clicked");
+    }
  
   return (
     <>
       <img src="https://images.pexels.com/photos/372787/pexels-photo-372787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
       
-      <input type="text" onFocus={()=>{
-        console.log("Focused on the text box");
-      }}  onBlur={()=>{
-        console.log("Out of Textbox");
-      }}/>
+      <input type="text"  />
 
       <button  onDoubleClick={(e)=>{
         alert("Button CLicked")
         console.log("Clicked");
-      }}>Play</button>
+      }}    >Play</button>
 
       <p onCopy={()=> { alert( "Copied")}} >Hello Everyone</p>
       <div>
-      <form action="">
-        <input type="text" />
-        <button></button>
-      </form>
+      <form onSubmit={handleSubmit}>
+      <label>
+        Enter something:
+        <input type="text"   onChange={handleInputChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
       </div>
      </>
 
