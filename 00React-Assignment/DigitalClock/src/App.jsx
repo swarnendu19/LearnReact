c75a2970ef48909d15ed96f2eb52233bcccf363c
Swarnendu <swarnendumaity633@gmail.com>
2023-11-29 20:36:34 +0530

    Assignment

--- 00React-Assignment/DigitalClock/src/App.jsx
+++ 00React-Assignment/DigitalClock/src/App.jsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTime,setCurrentTime] = useState(new Date())
+  const[isRunning,setIsRunning]= useState(true)
+  const[intervalId,setIntervalId]= useState(null)
+  const updateCurrentTime =()=>{
+    setCurrentTime(new Date())
+  }
+  const startTimer =()=>{
+    setIsRunning(true)
+    const id = setInterval(updateCurrentTime, 1000);
+    setIntervalId(id);  }
+
+  const stopTimer= ()=>{
+    setIsRunning(false)
+    clearInterval(intervalId);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Digital Clock</h1>
+      <p>{currentTime.toLocaleTimeString()}</p>
+      <button onClick={startTimer} disabled={isRunning}>
+        Start Timer
+      </button>
+      <button onClick={stopTimer} disabled={!isRunning}>
+        Stop Timer
+      </button>
+    </div>
     </>
   )
 }
-
 export default App
