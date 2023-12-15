8810ae6939e9719c3b7255892bd5c4643d57f2bd
Swarnendu <swarnendumaity633@gmail.com>
2023-12-15 19:01:12 +0530

    Assignment

--- 00React-Assignment/Assignment2/ResumeBuilder/component/Resume.jsx
+++ 00React-Assignment/Assignment2/ResumeBuilder/component/Resume.jsx
@@ -3,7 +3,7 @@ import Experience from './Experience';
 import Education from './Education';
 import Skills from './Skills';
 import Border from './Border';
-
+ 
 function Resume({data}) {
   return (
     <>
@@ -11,12 +11,15 @@ function Resume({data}) {
      <h1>My Resume</h1>
      <Border>
      <Experience experience={data.experience}/>
+      <button>Edit</button>
      </Border>
      <Border>
      <Education education={data.education}/>
+     <button>Edit</button>
      </Border>
      <Border>
      <Skills skills={data.skills } />
+     <button>Edit</button>
      </Border>
      
     </div>
